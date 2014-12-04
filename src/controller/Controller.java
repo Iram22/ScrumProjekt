ceccba054612f764753889d65329a716e1467ac8
Ahmed <Ahmed@10.50.131.144>
2014-12-04 19:07:45 +0100

    Controller kom ikke med ved sidste Commit & Push

---
+++ src/controller/Controller.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.util.ArrayList;
+import javax.swing.DefaultListModel;
+
+/**
+ *
+ * @author Ahmed
+ */
+public class Controller {
+
+    public Controller()
+    {
+
+    }
+
+    public void flytFag(Object valgteFag, DefaultListModel modelFra, DefaultListModel modelTil)
+    {
+        boolean fundet = modelFra.removeElement(valgteFag);
+        if (fundet)
+        {
+            modelTil.addElement(valgteFag);
+        }
+    }
+
+    public ArrayList hentValgfagFraDB()
+    {
+        ArrayList valgfag = new ArrayList();
+        valgfag.add("C#");
+        valgfag.add("Android");
+        valgfag.add("Python");
+        valgfag.add("Dansk");
+
+        return valgfag;
+    }
+
+}
